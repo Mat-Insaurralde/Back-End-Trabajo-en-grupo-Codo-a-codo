36f249e47eff87a9790330eeb6f1f3b081c430d2
Javier Matias Insaurralde <javiermatias115@gmail.com>
2024-06-30 21:14:27 -0300

    actualizo app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 import express from "express";
 //Importamos variables de entorno
-import 'dotenv/config';
+import env from 'dotenv';
 
 import authRoutes from './routes/authRoutes.js';
 
@@ -10,29 +10,26 @@ import userRoutes from './routes/userRoutes.js'
 
 import bookRoutes from './routes/bookRoutes.js';
 
+env.config();
 
-
-const app = express();
-             
-             //Puerto del host si no existe usa el 3000
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+app.use(express.json());             
+           
+app.use(express.urlencoded({ extended: true }));
 
 
-const corsOptions = {
-    origin: process.env.ORIGIN_CORS,
+
+app.use(
+    cors( { 
+    origin: [process.env.ORIGIN_CORS],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 200 // algunos navegadores antiguos (IE11, varios SmartTVs) chokan con 204
-  };
-  
-  app.use(cors(corsOptions));
-  
-  app.options('*', cors(corsOptions));
-
-
-
-app.use(express.json());
+      }));
+   
 
 
 
